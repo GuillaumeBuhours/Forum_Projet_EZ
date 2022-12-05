d1fdcd6874ff8f7fceb47fc87b4022bfac18720f
GuillaumeB <108664412+GuillaumeBuhours@users.noreply.github.com>
2022-12-05 14:36:02 +0100

    ez

--- accueil.php
+++ accueil.php
@@ -7,19 +7,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forum Project EZ</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-    <header>
+    <header id="head">
         <div id="banniere">
             <button id="inscription">inscription</button>
             <button id="connection">Connection</button>
             <button id="deconnection">Deconnection</button>
             <button id="profil">Profil</button>
-            <h1 id="titre">LE FORUM DES GEEKEZ</h1>
         </div>
+        <h1 id="titre">LE FORUM DES GEEKEZ</h1>
     </header>
-    <hr>
+    <br><hr>
+    <form method="" action="" id="inscrire">
+        <input type="text" placeholder="E-Mail" name="mail">
+        <input type="text" placeholder="Pseudo" name="pseudo">
+        <input type="text" placeholder="mot de passe" name="mdp">
+        <button>S'inscrire</button>
+    </form>
+    <form method="" action="" id="connect"> 
+        <input type="text" placeholder="Pseudo" name="name1">
+        <input type="text" placeholder="mot de passe" name="mdp1">
+        <button>Se Connecter</button>
+    </form>
     <table id="conteneur">
         <tr id="index">
             <th class="ligne">Icone</th>
