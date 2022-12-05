89252dae5adcc15aa647f5c57510be42b5604b67
GuillaumeB <108664412+GuillaumeBuhours@users.noreply.github.com>
2022-12-05 14:05:40 +0100

    izi

--- accueil.php
+++ accueil.php
@@ -1,3 +1,34 @@
 <?php 
-echo 'Hello World !';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum Project EZ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div id="banniere">
+            <button id="inscription">inscription</button>
+            <button id="connection">Connection</button>
+            <button id="deconnection">Deconnection</button>
+            <button id="profil">Profil</button>
+            <h1 id="titre">LE FORUM DES GEEKEZ</h1>
+        </div>
+    </header>
+    <hr>
+    <table id="conteneur">
+        <tr id="index">
+            <th class="ligne">Icone</th>
+            <th class="ligne">Nom du Topic</th>
+            <th class="ligne">Créateur</th>
+            <th class="ligne">Nombre de Msg</th>
+            <th class="ligne">Nombre de vues</th>
+        </tr>
+    </table>
+    <script src="index.js"></script>
+</body>
+</html>
