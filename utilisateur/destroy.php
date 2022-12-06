e34672c2a72827308a06a0fe4f536a3474111a61
Damien-Erhard <erharddamien@gmail.com>
2022-12-06 13:44:54 +0100

    deco et suppr

--- utilisateur/destroy.php
+++ utilisateur/destroy.php
@@ -1,3 +1,55 @@
 <?php
-include_once "index.php";
-?>
+
+$dbhost = 'localhost';
+$dbname = 'forum_users';
+$dbuser = 'root';
+$dbpass = '';
+try{
+
+    $bdd = new PDO( 'mysql:host='.$dbhost.';dbname='.$dbname.'', $dbuser, $dbpass );
+} catch( Exception $e ){
+    die( 'Erreur : ' . $e->getMessage() );
+}
+
+	$id="";	
+
+	try
+	{	
+
+		$bdd=new PDO('mysql:host=localhost;dbname=forum_users','root','');
+
+	}
+	catch(Exception $e)
+	{
+	
+		die('<strong>Erreur détectée !!! </strong>' . $e->getMessage());
+	}
+	
+	$requete=$bdd->prepare('SELECT * FROM utilisateurs WHERE id=id') or die (print_r($bdd->errorInfo()));
+	
+	$requete->execute(array($_POST['']));
+
+	while ($data=$requete->fetch())
+	{
+		$id=$data[''];
+	}
+	if ($id=="")
+	{	
+	
+		echo 'Impossible de supprimer les donn�es. Le nom de ce membre n\'est pas enregistr� dans la base.';
+	}
+	else
+	{
+
+		$requete=$bdd->prepare('DELETE FROM utilisateurs WHERE id=:id') or die (print_r($bdd->errorInfo()));
+
+		$requete->execute(array(
+		'nom' => $_POST[''],
+		));
+	
+		echo 'Les informations concernant le nouveau membre ont bien �t� supprim�es de la base.';
+    session_destroy();
+	}
+
+	$requete->closeCursor();
+	?>	
