1639ab157fb5712ce5e6c1805e97eb6cd2ab7b02
GuillaumeB <108664412+GuillaumeBuhours@users.noreply.github.com>
2022-12-06 10:00:17 +0100

    ez

--- utilisateur/create.php
+++ utilisateur/create.php
@@ -1,18 +1,3 @@
 <?php
 include_once "../forum/index.php";
-
-
-$pdo = pdo_connect();
-
-    if(isset($_POST['submit']) 
-    && !empty($_POST['mail']) 
-    && !empty($_POST['pseudo']) 
-    && !empty($_POST['mdp'])){
-        $_REQUEST = $pdo->prepare("INSERT INTO utilisateurs VALUE(NULL, :mail, :pseudo, :mdp)");
-        $_REQUEST->execute([
-            'mail' => $_POST['mail'],
-            'pseudo' => $_POST['pseudo'],
-            'mdp' => $_POST['mdp']
-        ]);
-        header("refresh:5; url=../accueil.php");
-    }
+?>
--- utilisateur/destroy.php
+++ utilisateur/destroy.php
@@ -1,17 +1,3 @@
 <?php
 include_once "index.php";
-
-$pdo = pdo_connect();
-
-if (isset($_POST["id"])) {
-
-    $id=$_POST["id"];
-
-    $delete="DELETE FROM utilisateurs WHERE id=$id";
-
-    if($pdo($delete))
-        echo '{"deleted":"true"}';
-    else
-        echo '{"deleted":"false"}';
-
-}
+?>
