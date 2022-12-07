050bff3306db18cef70140bd9a275d3658f4b72e
Damien-Erhard <erharddamien@gmail.com>
2022-12-07 14:21:14 +0100

    readall_topic

---
+++ forum/readall_topic.php
@@ -0,0 +1,15 @@
+<?php
+
+$mysqli=mysqli_connect("localhost","forum_users","","");
+
+$sql="select * from topic ORDER BY pseudo DESC";
+
+$result=mysqli_query($mysqli,$sql);
+
+$all=mysqli_fetch_all($result,MYSQLI_ASSOC);
+
+$jsonall=json_encode($all,JSON_PRETTY_PRINT);
+
+echo $jsonall;
+
+?>
